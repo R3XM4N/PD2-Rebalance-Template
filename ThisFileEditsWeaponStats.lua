eeb7e870b40f02944c3f68d1eb979ac9644f759d
Rexxi <trolove321@gmail.com>
2023-04-28 21:13:33 +0200

    Fixed an opsie

--- ThisFileEditsWeaponStats.lua
+++ ThisFileEditsWeaponStats.lua
@@ -16,7 +16,7 @@ Hooks:PostHook( WeaponTweakData, "init", "ThisFileEditsWeaponStats", function(se
     self.awp.CLIP_AMMO_MAX = 10
 
     --m4a1 - car 4
-    self.new_m4.fire_mode_data.fire_rate = 700
+    self.new_m4.fire_mode_data.fire_rate = 60 / 700
     self.new_m4.CLIP_AMMO_MAX = 30
     self.new_m4.AMMO_MAX = 180
     self.new_m4.stats.spread = 20
@@ -25,29 +25,29 @@ Hooks:PostHook( WeaponTweakData, "init", "ThisFileEditsWeaponStats", function(se
     self.new_m4.damage_falloff = ASSAULT_FALL_LOW
 
     --heavy eagle - scar
-    self.scar.fire_mode_data.fire_rate = 650
+    self.scar.fire_mode_data.fire_rate = 60 / 650
     self.scar.CLIP_AMMO_MAX = 20
     self.scar.damage_falloff = ASSAULT_FALL_LOW
 
     --clarion - famas
-    self.famas.fire_mode_data.fire_rate = 1100
+    self.famas.fire_mode_data.fire_rate = 60 / 1100
     self.famas.damage_falloff = ASSAULT_FALL_LOW
 
     --smgs
 
     --mark10 - mac11 -- mac10pdid
-    self.mac10.fire_mode_data.fire_rate = 1200
+    self.mac10.fire_mode_data.fire_rate = 60 / 1200
     self.mac10.CLIP_AMMO_MAX = 32
     self.mac10.AMMO_MAX = 128
     self.mac10.damage_falloff = ASSAULT_FALL_LOW
 
     --p90 kobus
-    self.p90.fire_mode_data.fire_rate = 1100
+    self.p90.fire_mode_data.fire_rate = 60 / 1100
     self.p90.damage_falloff = ASSAULT_FALL_LOW
 
     --ump45 schakalid jackal
     self.schakal.damage_falloff = ASSAULT_FALL_LOW
-    self.schakal.fire_mode_data.fire_rate = 750
+    self.schakal.fire_mode_data.fire_rate = 60 / 750
 
     --
 
